39d21d241b7b1293590714fc73277c022b24ee74
kevinbell.dev <KevinBell.dev@gmail.com>
2024-08-16 19:43:34 -0300

    clase 25 1 hs

---
+++ PWI/react-proyecto-3 (Clase 25)/src/Components/ColorCard/ColorCard.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+
+
+export const ColorCard = ({tarjeta}) => {
+    const {likes, fecha, colores} = tarjeta
+    return (
+        <>
+        <div className="color-card">
+            <div className="colors">
+                <div className="color" style = {{backgroundColor : colores[0]}}></div>
+                <div className="color" style = {{backgroundColor : colores[1]}}></div>
+                <div className="color" style = {{backgroundColor : colores[2]}}></div>
+                <div className="color" style = {{backgroundColor : colores[3]}}></div>
+            </div>
+            <div className="color-card-controls">
+                <button className="btn-like"><i className="bi bi-heart"></i> <span>{likes}</span></button>
+                <span className="time">{fecha}</span>
+            </div>
+        </div>
+        </>
+    )
+}
+export const ColorCardList = ({colors_info}) =>{
+    return(
+        <>
+            {
+                colors_info.map((color_info) => <ColorCard tarjeta={color_info}/>)
+            }
+        </>
+    )
+}
